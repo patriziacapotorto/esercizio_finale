bdf7d46599dd10862071e8014e0636ac3d80898f
CAPOTORTO\Patrizia <patriziacapotorto1@gmail.com>
2017-03-07 16:11:52 +0100

    invio utente per id con json

--- server/utenti/utenti.js
+++ server/utenti/utenti.js
@@ -6,12 +6,24 @@ var listautenti = require('./database.js');
 //LE ROTTE PARTONO DA /UTENTI
 // PAGINA PRINCIPALE UTENTI
 router.get('/', function(req, res) {
-  res.json(listautenti);
+  res.status(200).json(listautenti);
 });
 
 // DETTAGLIO DI UN UTENTE
-// router.get('/:id', function(req, res) {
-//   res.send('Dettaglio dell utente con id :' + req.params.id);
-// });
+router.get('/:id', function(req, res) {
+  var id = req.params.id;
+  var utente ={};
+  var utente = listautenti.find(function(el){
+    return el.id == id;
+  });
+  res.status(200).json(utente);
+  //loop1:
+  // for(let i =0; i < listautenti.length; i++){
+  //   if(id == listautenti[i].id){
+  //     utente = listautenti[i];
+  //     break loop1;
+  //   }
+  // }
+});
 
 module.exports = router;
